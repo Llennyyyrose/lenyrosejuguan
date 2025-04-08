490419789120f1af8c10fd1ae23c7e8dc4c712a7
Llennyyyrose <145463067+Llennyyyrose@users.noreply.github.com>
2025-04-08 16:39:16 +0800

    Activity4

---
+++ about.php
@@ -0,0 +1,16 @@
+ <!-- About section -->
+ <section id="about">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <h3>dfadsfa</h3>
+          <h2>adfasdfa</h2>
+          <p>adfsadsfads</p>
+          <a href="#">Shucks</a>
+        </div>
+        <div class="col-lg-6">
+          <img src="OIP.jpeg" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <img src="cosmos-flowers.jpg" alt="logo" class="hays">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <a href="#" class="btn btn-bembang">BEMBANG!</a>
+      </div>
+    </div>
+  </nav>
+
+  <!--HERO SECXTION-->
+  <div class="banner d-flex align-items-center"  id="banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>centLorem ipsum dolor sit amet consectetur adipisicing.</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque earum enim consequatur iusto nam? Saepe
+            eius similique deserunt officiis amet.</p>
+            <a href="#" class="btn btn-bembang">BEMBANG!</a>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+
+  <!-- services -->
+   <section id="services">
+    <div class="container">
+      <h2 class="text-center">asdfasddfasdf</h2>
+      <div class="row">
+        <div class="col d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <img src="gloves.png" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <img src="gloves.png" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <img src="gloves.png" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <img src="gloves.png" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <img src="gloves.png" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col d-flex justify-content-center">
+          <div class="card" style="width: 18rem;">
+            <img src="gloves.png" class="card-img-top mx-auto" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </section>
+
+   <!--reviews-->
+   <section id="reviews">
+    <div class="container">
+      <div class="text-center text-light">
+        <h2>REVIEWS</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat necessitatibus est repudiandae ducimus eaque, incidunt eligendi atque minus itaque accusantium laboriosam perferendis porro harum dignissimos tempore, dolorum provident, et fugit!</p>
+      </div>
+    </div>
+    <!--row starts here-->
+    <div class="row row-cols-lg-4">
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="testimonial">
+          <div class="d-flex">
+            <img src="labubu.jpg" alt="">
+            <div class="text-light ms-3">
+              <h5>client name</h5>
+              <h6>@client name</h6>
+            </div>
+          </div>
+          <p class="text-light mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, quasi asperiores, exercitationem aut, porro voluptatum enim quas magni deserunt temporibus officia explicabo ipsum assumenda dolore earum modi aliquam cupiditate.</p>
+        </div>
+      </div>
+    </div>
+   </section>
+ <!-- faq-->
+ <section id="faq">
+  <div class="container">
+    <h2 class="text-center"> Frequently Asked Questions</h2>
+    <div class="faq-section">
+      <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+              Accordion Item #1
+            </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="flush-headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ </section>
+
+<!--FOOTER -->
+<footer class="footer text-white" id="footer">
+<div class="container">
+<div class="row al">
+<div class="col-md-3 col-sm-6 text-lg-start text-center">
+  <a class="navbar-brand" href="#">
+      <img src="labubu.jpg" alt="Logo" height="60px">
+  </a>
+  <h5 class="mt-4">About Us</h5>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+</div>
+<div class="col-md-3 col-sm-6 text-lg-start ps-lg-5 ps-0 text-center ">
+  <h5>Services</h5>
+  <ul class="list-unstyled">
+      <li><a href="#">Service 1</a></li>
+      <li><a href="#">Service 2</a></li>
+      <li><a href="#">Service 3</a></li>
+      <li><a href="#">Service 4</a></li>
+      <li><a href="#">Service 5</a></li>
+  </ul>
+</div>
+<div class="col-md-3 col-sm-6 text-lg-start text-center">
+  <h5>Contact Us</h5>
+  <p>123 Street Name,<br>City, Country</p>
+  <p>Email: example@example.com<br>Phone: +1234567890</p>
+</div>
+<div class="col-md-3 col-sm-6 text-lg-start  text-center">
+  <h5>Newsletter</h5>
+  <form>
+      <div class="input-group mb-3">
+          <input type="text" class="form-control" style="border-radius: 1px;" placeholder="Email"
+              aria-label="email" aria-describedby="button-addon2">
+          <button class="btn btn-outline-light" type="button" id="button-addon2">Subscribe</button>
+      </div>
+  </form>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+</div>
+</div>
+</div>
+</footer>
+
+   </section>
+</body>
+
+</html>
